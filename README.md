c87c77e14705fbfcd01e39a1ad40aab686dc52c6
Your Name <louisisaacdf@gmail.com>
2024-01-11 08:46:39 +0000

    Plugin initialization

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# get-woocommerce-product-category
+# Get WooCommerce Product Category
+
+## Description
 A WordPress plugin that returns the main category of a given WooCommerce product.
---
+++ get-woocommerce-product-category.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package GetProductCategoryPlugin
+ */
+/*
+Plugin Name: Get WooCommerce Product Category
+Plugin URI: https://github.com/louisisaacdiouf/get-woocommerce-product-category
+Description: A WordPress plugin that returns the main category of a given WooCommerce product.
+Version: 1.0.0
+Author: Louis Isaac Diouf
+Author URI: https://github.com/louisisaacdiouf
+Licence: GPLv2
+*/
