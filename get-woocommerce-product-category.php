e1dbf8ed1e2b50d2897ad90908c3e1e19d465cc1
Your Name <louisisaacdf@gmail.com>
2024-01-11 11:26:51 +0000

    Plugin initialization

--- get-woocommerce-product-category.php
+++ get-woocommerce-product-category.php
@@ -11,3 +11,29 @@ Author: Louis Isaac Diouf
 Author URI: https://github.com/louisisaacdiouf
 Licence: GPLv2
 */
+
+function_exists('add_action') or die("Avada kedavra!");
+
+add_action('wp_ajax_get_product_category', 'get_product_category_callback');
+add_action('wp_ajax_nopriv_get_product_category', 'get_product_category_callback');
+
+function get_product_category_callback() {
+  if (isset($_POST['product_id'])) {
+      $product_id = intval($_POST['product_id']);
+      $product = wc_get_product($product_id);
+      
+      if ($product) {
+          $categories = wp_get_post_terms($product->get_id(), 'product_cat');
+          
+          // Retournez la première catégorie
+          if (!empty($categories)) {
+              $first_category = array_shift($categories);
+              echo $first_category->name;
+          } else {
+              echo "";
+          }
+      }
+  }
+  
+  die();
+}
