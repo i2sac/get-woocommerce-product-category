79dda56aed713d5d87e372c652a49e1b2f44d617
Louis Isaac DIOUF <61230778+i2sac@users.noreply.github.com>
2025-09-05 12:03:29 +0000

    Update get-woocommerce-product-category.php

--- get-woocommerce-product-category.php
+++ get-woocommerce-product-category.php
@@ -4,8 +4,8 @@
  */
 /*
 Plugin Name: Get WooCommerce Product Category
-Plugin URI: https://github.com/louisisaacdiouf/get-woocommerce-product-category
-Description: A WordPress plugin that returns the main category of a given WooCommerce product.
+Plugin URI: https://github.com/i2sac/get-woocommerce-product-category
+Description: A WordPress plugin that returns the main category of a given WooCommerce product via a JS POST request.
 Version: 1.0.0
 Author: Louis Isaac Diouf
 Author URI: https://github.com/i2sac
